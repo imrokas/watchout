bec33bd684d5089808d124bb505d8b4837c6d9c3
imrokas <rokas@imrokas.com>
2015-12-05 14:45:49 -0500

    optimized code for tween

--- client/watchout.js
+++ client/watchout.js
@@ -3,18 +3,34 @@ var board = d3.select('body').append('svg');
 
 var height = 600;
 var width = 800;
+var score = 0;
+var highScore = 0;
+var collision = 0;
 
 var nEnemies = 30;
 var asteroidData = [];
 var asteroids; 
+var once = function(func) {
+  var ran = false, 
+      memo;
+  return function() {
+    if (ran) 
+      return memo;
+    ran = true;
+    memo = func.apply(this, arguments);
+    func = null;
+    return memo;
+  };
+};
 
 var getRandomX = function() {
-  return Math.max(0, Math.random() * width - 50);
+  return Math.max(0, Math.random() * width - 25);
 };
 
 var getRandomY = function() {
-  return Math.max(0, Math.random() * height - 50);
+  return Math.max(0, Math.random() * height - 25);
 };
+
 
 for(var i = 0; i < nEnemies; i++)   {
   asteroidData.push({
@@ -32,16 +48,16 @@ asteroids = board.selectAll('image')
         .attr('xlink:href', 'asteroid.png')
         .attr('x', function(d) { return d.x; })
         .attr('y', function(d) { return d.y; })
+        .attr('r', function(d) { return d.r; })
         .attr('height', function(d) { return d.r*2; })
         .attr('width', function(d) { return d.r*2; });
 
 
 var moveAsteroids = function() {
-  asteroids.transition()
-    .attr('x', function(d) {d.x = getRandomX(); return d.x})
-    .attr('y', function(d) {d.y = getRandomY(); return d.y})
-    .duration(1000)
-    .tween('custom', checkCollision);
+  asteroids
+    .transition()
+    .duration(2000)
+    .tween('custom', tweenWithCollisions)
 }
 
 setInterval(moveAsteroids, 1000);
@@ -65,14 +81,51 @@ board.on('mousemove', function(){
 ;})
 
 var checkCollision =function(enemy, callback)  {
-  var player = window.player.data()[0];
-  var radius = parseFloat(enemy.r) + 10;
-  var xDiff = parseFloat(enemy.x) - player.x
-  var yDiff = parseFloat(enemy.y) - player.y
-
+  var player = d3.select('.player');//window.player.data()[0];
+  var radius = parseFloat(enemy.attr('r')) + parseFloat(player.attr('r'));
+  var xDiff = parseFloat(enemy.attr('x'))- parseFloat(player.attr('cx'));
+  var yDiff = parseFloat(enemy.attr('y')) - parseFloat(player.attr('cy'));
   var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2));
- 
-  if (separation < radius*2) {
-    console.log(radius);
+
+  if (separation < radius) {
+    score = 0;
+    d3.select('.collisions span').text(++collision);
   }
 }
+
+var tweenWithCollisions = function(enemy, i) {
+  var enemy1 = d3.select(this);
+  //console.log('inside ', enemy, ' index ', i);
+  return  function(t) {
+    checkCollision(enemy1);
+    startPos = {
+      x: enemy1.attr('x'),
+      y: enemy1.attr('y')
+    }
+    endPos = {
+      x: once(getRandomX)(),
+      y: once(getRandomY)()
+    }
+    //console.log('x=', endPos.x, ' y=', endPos.y, ' i =', i);
+    //console.log(startPos);
+    enemyNextPos = {
+      x: parseFloat(startPos.x) + parseFloat((endPos.x - startPos.x)*t),
+      y: parseFloat(startPos.y) + parseFloat((endPos.y - startPos.y)*t)
+    }
+    //console.log(enemyNextPos.x);
+    enemy1.attr('x', enemyNextPos.x);
+    enemy1.attr('y', enemyNextPos.y);
+  }
+}
+
+
+var updasteScore = function() {
+  score++;
+  if(score > highScore) {
+   highScore = score;
+  }
+  d3.select('.current span').text(score);
+  d3.select('.high span').text(highScore);
+}
+
+setInterval(updasteScore, 50);
